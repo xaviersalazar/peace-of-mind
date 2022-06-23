eb96955800f56bbfc14fd8bb16ca4e9fb6feb984
Xavier Salazar <xavierssalazar92@gmail.com>
2022-06-23 12:33:52 -0500

    configuring font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <h1 className="ext-3xl font-bold underline">peace of mind</h1>;
+  return (
+    <div>
+      <h1 className="text-4xl font-extra-bold">Welcome to,</h1>
+      <h1 className="text-3xl font-extra-light">Peace of Mind</h1>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      "work-sans": ['"Work Sans"', "sans-serif"],
+    },
+    fontWeight: {
+      "extra-light": 200,
+      light: 300,
+      regular: 400,
+      medium: 500,
+      bold: 700,
+      "extra-bold": 800,
+    },
     extend: {},
   },
   plugins: [],
