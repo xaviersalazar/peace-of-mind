977f7e9c11962712ce44303a703d502d210b2074
Xavier Salazar <xavierssalazar92@gmail.com>
2022-07-10 11:53:18 -0500

    adjusting footer for diff screens

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,21 +3,21 @@ import { Footer as DaisyFooter } from "react-daisyui";
 const { Title } = DaisyFooter;
 
 export const Footer = () => (
-  <DaisyFooter className="p-10 bg-primary relative">
-    <div>
+  <DaisyFooter className="p-10 bg-primary relative lg:px-20 lg:py-10 xl:px-28">
+    <div className="md:w-[75%] lg:w-[50%]">
       <img
-        className="w-[84px]"
+        className="w-[98px] lg:w-[128px]"
         src={`${process.env.PUBLIC_URL}/assets/logo_black.png`}
         alt="background"
       />
-      <p className="mb-3 text-xs font-light">
+      <p className="text-xs font-light mb-0">
         Open every day so we can work around your schedule because life isn’t
         always easy!
       </p>
       <p className="text-xs font-light">10% discount for all military!</p>
     </div>
-    <div className="gap-1">
-      <Title className="mb-0">Location</Title>
+    <div className="gap-1 self-end">
+      <Title className="text-base mb-0">Location</Title>
       <p className="text-xs font-light">
         602 N Lower Broadway St Corpus Christi, TX 78401
       </p>
