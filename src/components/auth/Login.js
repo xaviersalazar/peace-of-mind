c7792664a0bf27f26a9b5e7bfa4049166a8e6293
Xavier Salazar <xavierssalazar92@gmail.com>
2022-11-09 19:14:38 -0600

    adding login form state

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,55 +1,103 @@
-const Login = () => (
-  <div className="relative top-0 px-10 pt-0 pb-10 lg:px-20 xl:px-[7rem] h-[100vh] overflow-hidden">
-    <div className="z-50 relative mt-12">
+import { useState } from "react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+const Login = () => {
+  const [form, setForm] = useState(initialState);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { email, password } = form;
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const onSubmit = () => {
+    console.log(form);
+  };
+
+  return (
+    <div className="relative top-0 px-10 pt-0 pb-10 lg:px-20 xl:px-[7rem] h-[100vh] overflow-hidden">
+      <div className="z-50 relative mt-12">
+        <img
+          className="w-[128px] mx-auto mb-12"
+          src={`${process.env.PUBLIC_URL}/logo-black.png`}
+          alt="background"
+        />
+        <div
+          className="mt-4 mx-auto bg-slate-50 rounded-2xl p-8 md:w-3/4 lg:w-7/12 xl:w-6/12"
+          id="login-form"
+        >
+          <h1 className="text-5xl font-bold text-center mb-8">Login</h1>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text font-light">Email address</span>
+            </label>
+            <input
+              name="email"
+              type="text"
+              className="input w-full h-10 font-light rounded-lg"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text font-light">Password</span>
+            </label>
+            <div className="relative">
+              <input
+                name="password"
+                type={showPassword ? "text" : "password"}
+                className="input w-full h-10 font-light rounded-lg"
+                value={password}
+                onChange={onChange}
+              />
+              {showPassword ? (
+                <button onClick={() => setShowPassword(!showPassword)}>
+                  <FiEye className="absolute top-2 right-4 h-6" />
+                </button>
+              ) : (
+                <button onClick={() => setShowPassword(!showPassword)}>
+                  <FiEyeOff className="absolute top-2 right-4 h-6" />
+                </button>
+              )}
+            </div>
+          </div>
+          <button
+            className="btn-primary w-full mt-8 p-2 rounded-lg text-slate-700 font-light"
+            onClick={onSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
       <img
-        className="w-[128px] mx-auto mb-12"
-        src={`${process.env.PUBLIC_URL}/logo-black.png`}
+        className="md:hidden w-[100vw] absolute left-0 bottom-0 z-0"
+        src={`${process.env.PUBLIC_URL}/wave-bg.svg`}
         alt="background"
       />
-      <div
-        className="mt-4 mx-auto bg-slate-50 rounded-2xl p-8 md:w-3/4 lg:w-7/12 xl:w-6/12"
-        id="login-form"
-      >
-        <h1 className="text-5xl font-bold text-center mb-8">Login</h1>
-        <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text font-light">Email address</span>
-          </label>
-          <input
-            type="text"
-            className="input w-full h-10 font-light rounded-lg"
-          />
-        </div>
-        <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text font-light">Password</span>
-          </label>
-          <input
-            type="text"
-            className="input w-full h-10 font-light rounded-lg"
-          />
-        </div>
-        <button className="btn-primary w-full mt-8 p-2 rounded-lg text-slate-700 font-light">
-          Submit
-        </button>
-      </div>
+      <img
+        className="hidden md:block xl:hidden w-[100vw] absolute left-0 bottom-0"
+        src={`${process.env.PUBLIC_URL}/wave-bg-md.svg`}
+        alt="background"
+      />
+      <img
+        className="hidden xl:block w-[100vw] absolute left-0 bottom-0"
+        src={`${process.env.PUBLIC_URL}/wave-bg-lg.svg`}
+        alt="background"
+      />
     </div>
-    <img
-      className="md:hidden w-[100vw] absolute left-0 bottom-0 z-0"
-      src={`${process.env.PUBLIC_URL}/wave-bg.svg`}
-      alt="background"
-    />
-    <img
-      className="hidden md:block xl:hidden w-[100vw] absolute left-0 bottom-0"
-      src={`${process.env.PUBLIC_URL}/wave-bg-md.svg`}
-      alt="background"
-    />
-    <img
-      className="hidden xl:block w-[100vw] absolute left-0 bottom-0"
-      src={`${process.env.PUBLIC_URL}/wave-bg-lg.svg`}
-      alt="background"
-    />
-  </div>
-);
+  );
+};
 
 export default Login;
