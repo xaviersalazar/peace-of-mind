7db6bd9ad8a8f3271a80d617f7059b1203da458a
Xavier Salazar <xavierssalazar92@gmail.com>
2022-12-05 14:50:30 -0600

    adding react-hook-form to login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,25 +1,39 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { FiEye, FiEyeOff, FiAlertCircle, FiXCircle } from "react-icons/fi";
+import { useForm } from "react-hook-form";
+import {
+  FiEye,
+  FiEyeOff,
+  FiAlertCircle,
+  FiXCircle,
+  FiAtSign,
+  FiUser,
+} from "react-icons/fi";
 import { AnimatePresence, motion } from "framer-motion";
 import { useAuth } from "../context/Auth";
-import { isEmpty } from "lodash";
 import { Spinner } from "../shared";
-
-const initialState = {
-  name: "",
-  email: "",
-  password: "",
-};
 
 const Login = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const { signUp, signIn } = useAuth();
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { isValid, errors },
+    reset,
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
 
   const isLogin = pathname === "/auth/login";
 
-  const [form, setForm] = useState(initialState);
   const [showPassword, setShowPassword] = useState(false);
   const [isLoggingIn, setIsLoggingIn] = useState(false);
   const [error, setError] = useState(null);
@@ -32,40 +46,17 @@ const Login = () => {
     }
   }, [error]);
 
-  const onChange = (event) => {
-    const { name, value } = event.target;
-
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
   const onSubmit = async () => {
-    const { name, email, password } = form;
-
+    const { name, email, password } = getValues();
     setIsLoggingIn(true);
 
     if (isLogin) {
-      if (isEmpty(email) || isEmpty(password)) {
-        setError({
-          msg: (
-            <div className="text-sm font-light">
-              <span className="block">Please fill out all fields</span>
-            </div>
-          ),
-          type: "warning",
-        });
-
-        setIsLoggingIn(false);
-        return;
-      }
-
       await signIn({
         email,
         password,
       }).then((res) => {
         if (res.data?.user) {
+          reset();
           navigate("/dashboard");
         }
 
@@ -82,20 +73,6 @@ const Login = () => {
         setIsLoggingIn(false);
       });
     } else {
-      if (isEmpty(name) || isEmpty(email) || isEmpty(password)) {
-        setError({
-          msg: (
-            <div className="text-sm font-light">
-              <span className="block">Please fill out all fields</span>
-            </div>
-          ),
-          type: "warning",
-        });
-
-        setIsLoggingIn(false);
-        return;
-      }
-
       const signUpData = {
         email,
         password,
@@ -108,6 +85,7 @@ const Login = () => {
 
       await signUp(signUpData).then((res) => {
         if (res.data?.user) {
+          reset();
           navigate("/dashboard");
         }
 
@@ -125,8 +103,6 @@ const Login = () => {
       });
     }
   };
-
-  const { name, email, password } = form;
 
   return (
     <div className="relative top-0 px-10 pt-0 pb-10 lg:px-20 xl:px-[7rem] h-[100vh] overflow-hidden">
@@ -151,86 +127,124 @@ const Login = () => {
           src={`${process.env.PUBLIC_URL}/logo-black.png`}
           alt="background"
         />
-        <div
-          className="mt-4 mx-auto bg-slate-50 rounded-2xl p-8 md:w-3/4 lg:w-7/12 xl:w-6/12"
+        <form
           id="login-form"
+          className="mt-4 mx-auto bg-slate-50 rounded-2xl p-8 md:w-3/4 lg:w-7/12 xl:w-6/12"
+          onSubmit={handleSubmit(onSubmit)}
         >
           <h1 className="text-5xl font-bold text-center mb-8">
             {isLogin ? "Login" : "Sign Up"}
           </h1>
           {!isLogin && (
-            <div className="form-control w-full">
+            <div className="form-control w-full mb-2">
               <label className="label">
-                <span className="label-text font-light">Name</span>
+                <span className="label-text font-bold">Name</span>
               </label>
-              <input
-                name="name"
-                type="text"
-                className="input w-full h-10 font-light rounded-lg focus:outline-primary"
-                value={name}
-                onChange={onChange}
-              />
+              <div className="relative">
+                <input
+                  name="name"
+                  type="text"
+                  className="input w-full h-10 font-light text-slate-500 rounded-lg focus:outline-primary"
+                  {...register("name", {
+                    required: { value: true, message: "Name is required" },
+                  })}
+                />
+                <FiUser className="absolute right-4 top-3 text-slate-300" />
+              </div>
+              {errors?.name && (
+                <p className="text-red-300 font-light text-xs ml-1 mt-1 text-left">
+                  {errors.name.message}
+                </p>
+              )}
             </div>
           )}
-          <div className="form-control w-full">
+          <div className="form-control w-full mb-2">
             <label className="label">
-              <span className="label-text font-light">Email address</span>
+              <span className="label-text font-bold">Email address</span>
             </label>
-            <input
-              name="email"
-              type="text"
-              className="input w-full h-10 font-light rounded-lg focus:outline-primary"
-              value={email}
-              onChange={onChange}
-            />
+            <div className="relative">
+              <input
+                name="email"
+                type="text"
+                className="input w-full h-10 font-light text-slate-500 rounded-lg focus:outline-primary"
+                {...register("email", {
+                  required: { value: true, message: "Email is required" },
+                  pattern: {
+                    value: /^[a-z0-9.]{1,64}@[a-z0-9.]{1,64}$/i,
+                    message: "Please enter a valid email address",
+                  },
+                })}
+              />
+              <FiAtSign className="absolute right-4 top-3 text-slate-300" />
+            </div>
+            {errors?.email && (
+              <p className="text-red-300 font-light text-xs ml-1 mt-1 text-left">
+                {errors.email.message}
+              </p>
+            )}
           </div>
-          <div className="form-control w-full">
+          <div className="form-control w-full mb-2">
             <label className="label">
-              <span className="label-text font-light">Password</span>
+              <span className="label-text font-bold">Password</span>
             </label>
             <div className="relative">
               <input
                 name="password"
                 type={showPassword ? "text" : "password"}
-                className="input w-full h-10 font-light rounded-lg focus:outline-primary"
-                value={password}
-                onChange={onChange}
+                className="input w-full h-10 font-light text-slate-500 rounded-lg focus:outline-primary"
+                {...register("password", {
+                  required: { value: true, message: "Password is required" },
+                })}
               />
               {showPassword ? (
-                <button onClick={() => setShowPassword(!showPassword)}>
-                  <FiEye className="absolute top-2 right-4 h-6" />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  <FiEye className="absolute top-2 right-4 h-6 text-slate-300" />
                 </button>
               ) : (
-                <button onClick={() => setShowPassword(!showPassword)}>
-                  <FiEyeOff className="absolute top-2 right-4 h-6" />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  <FiEyeOff className="absolute top-2 right-4 h-6 text-slate-300" />
                 </button>
               )}
             </div>
+            {errors?.password && (
+              <p className="text-red-300 font-light text-xs ml-1 mt-1 text-left">
+                {errors.password.message}
+              </p>
+            )}
           </div>
           <motion.button
-            className="bg-primary w-full mt-8 p-2 rounded-lg text-slate-700 font-light"
-            whileHover={{
-              backgroundColor: "#f8fafc",
-              boxShadow:
-                "0px 0px 0px 2.5px #f8fafc, 0px 0px 0px 5px #10FFCB, 0px 0px 0px 10px #f8fafc, 0px 0px 0px 10.5px #10FFCB",
-              color: "#10FFCB",
-              transition: { duration: 0.3 },
-            }}
-            whileFocus={{
-              backgroundColor: "#f8fafc",
-              boxShadow:
-                "0px 0px 0px 2.5px #f8fafc, 0px 0px 0px 5px #10FFCB, 0px 0px 0px 10px #f8fafc, 0px 0px 0px 10.5px #10FFCB",
-              color: "#10FFCB",
-              transition: { duration: 0.3 },
-            }}
-            whileTap={{
-              scale: 0.9,
-            }}
-            disabled={isLoggingIn}
-            onClick={(e) => {
-              onSubmit();
-              e.currentTarget.blur();
-            }}
+            type="submit"
+            className="bg-primary w-full mt-8 p-2 rounded-lg text-slate-700 font-bold disabled:bg-slate-100 disabled:text-slate-300"
+            whileHover={
+              isValid && {
+                backgroundColor: "#f8fafc",
+                boxShadow:
+                  "0px 0px 0px 2.5px #f8fafc, 0px 0px 0px 5px #10FFCB, 0px 0px 0px 10px #f8fafc, 0px 0px 0px 10.5px #10FFCB",
+                color: "#10FFCB",
+                transition: { duration: 0.3 },
+              }
+            }
+            whileFocus={
+              isValid && {
+                backgroundColor: "#f8fafc",
+                boxShadow:
+                  "0px 0px 0px 2.5px #f8fafc, 0px 0px 0px 5px #10FFCB, 0px 0px 0px 10px #f8fafc, 0px 0px 0px 10.5px #10FFCB",
+                color: "#10FFCB",
+                transition: { duration: 0.3 },
+              }
+            }
+            whileTap={
+              isValid && {
+                scale: 0.9,
+              }
+            }
+            disabled={!isValid || isLoggingIn}
           >
             {isLoggingIn && <Spinner size="4" color="text-slate-400" />}
             Submit
@@ -250,7 +264,7 @@ const Login = () => {
               </Link>
             </p>
           )}
-        </div>
+        </form>
       </div>
       <img
         className="md:hidden w-[100vw] absolute left-0 bottom-0 z-0"
