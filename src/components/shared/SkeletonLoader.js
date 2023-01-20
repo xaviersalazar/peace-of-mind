31bf6760dee46e957439e4e9b98177ff7148e979
Xavier Salazar <xavierssalazar92@gmail.com>
2023-01-20 13:31:24 -0600

    updating loading skeleton

--- src/components/shared/SkeletonLoader.js
+++ src/components/shared/SkeletonLoader.js
@@ -3,8 +3,19 @@ const SkeletonLoader = () => (
     {[1, 2, 3, 4, 5, 6].map((item) => (
       <div
         key={item}
-        className="card border-none bg-slate-50 h-80 flex-1 basis-full md:basis-1/3 lg:basis-1/4 animate-pulse"
-      />
+        className="card border-none bg-slate-50 h-auto flex-1 basis-full md:basis-1/3 lg:basis-1/4 animate-[pulse_1s_infinite]"
+      >
+        <img
+          className="w-10/12 xl:w-8/12 rounded-xl mx-auto mt-12"
+          src={`${process.env.PUBLIC_URL}/placeholder-service.jpg`}
+          alt="placeholder"
+        />
+        <div className="card-body items-center text-center flex-none">
+          <div className="w-3/4 bg-slate-100 h-4 rounded-xl" />
+          <div className="w-2/4 bg-slate-100 h-4 rounded-xl" />
+          <div className="w-1/4 bg-slate-100 h-4 rounded-xl" />
+        </div>
+      </div>
     ))}
   </div>
 );
