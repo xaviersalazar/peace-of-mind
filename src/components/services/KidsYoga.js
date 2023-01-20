b3bd53c8396150d3139eef8b543ba444045afc77
Xavier Salazar <xavierssalazar92@gmail.com>
2023-01-20 15:56:40 -0600

    updating kids yoga

--- src/components/services/KidsYoga.js
+++ src/components/services/KidsYoga.js
@@ -1,7 +1,13 @@
 import { useQuery } from "@apollo/client";
+import classNames from "classnames";
 import { GET_SERVICES_BY_CATEGORY } from "../../graphql/queries";
-import { Error, ServiceCard, SkeletonLoader } from "../shared";
+import { Error, Section, ServiceCard, SkeletonLoader } from "../shared";
 import { IMG_PATH_PREFIXES } from "../utils/imgPathPrefixes";
+import { KIDS_YOGA_DATA } from "./kidsYogaData";
+
+const Text = ({ children }) => (
+  <p className="text-sm font-light text-slate-700 text-center">{children}</p>
+);
 
 const KidsYoga = ({ categoryId }) => {
   const { loading, error, data } = useQuery(GET_SERVICES_BY_CATEGORY, {
@@ -21,14 +27,65 @@ const KidsYoga = ({ categoryId }) => {
   const { servicesByCategory } = data;
 
   return (
-    <div className="flex flex-wrap gap-6" id="kids-yoga">
-      {servicesByCategory.map((service) => (
-        <ServiceCard
-          key={service.id}
-          service={service}
-          imgPathPrefix={IMG_PATH_PREFIXES.SERVICES}
-        />
-      ))}
+    <div id="kids-yoga">
+      <div className="flex flex-wrap gap-6">
+        {servicesByCategory.map((service) => (
+          <ServiceCard
+            key={service.id}
+            service={service}
+            imgPathPrefix={IMG_PATH_PREFIXES.SERVICES}
+          />
+        ))}
+      </div>
+      <div className="flex flex-wrap gap-6 my-12" id="kids-yoga-benefits">
+        {KIDS_YOGA_DATA.BENEFITS.map(({ title, text }) => (
+          <div
+            key={title}
+            className="card border-none rounded-2xl flex-1 basis-full md:basis-1/3 lg:basis-1/4"
+          >
+            <h1 className="text-3xl font-extra-bold lg:text-4xl">{title}</h1>
+            <hr className="w-2/4 md:w-6/12 xl:w-3/12 mx-auto my-4 border-slate-100" />
+            <Section>{text}</Section>
+          </div>
+        ))}
+      </div>
+      <div
+        className="card border-none rounded-2xl flex-1 basis-full md:basis-1/3 lg:basis-1/4"
+        id="expectations"
+      >
+        <div className="card-body items-center text-center flex-none">
+          <h1 className="card-title text-center justify-center text-3xl font-extra-bold lg:text-4xl">
+            What To Expect In Our Yoga Class
+          </h1>
+          <hr className="w-2/4 md:w-6/12 xl:w-3/12 mx-auto my-4 border-slate-100" />
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-6" id="ir-benefits">
+        {KIDS_YOGA_DATA.EXPECTATIONS.map(({ title, imgName, text }, i) => (
+          <div
+            key={i}
+            className={classNames(
+              "bg-slate-50 card border-none rounded-2xl flex-1 basis-full md:basis-1/3",
+              i === KIDS_YOGA_DATA.EXPECTATIONS.length - 1
+                ? "lg:flex-initial lg:max-w-1/2 lg:w-1/2 lg:mx-auto"
+                : "lg:basis-1/4"
+            )}
+          >
+            <figure>
+              <img
+                className="w-10/12 xl:w-8/12 rounded-xl mx-auto mt-12"
+                src={`${process.env.REACT_APP_BUCKETEER_URL}/public/services/${imgName}`}
+                alt="benefit"
+              />
+            </figure>
+            <div className="card-body items-center text-center flex-none">
+              <h1 className="card-title text-center justify-center">{title}</h1>
+              <hr className="w-2/4 md:w-6/12 xl:w-3/12 mx-auto my-1 border-slate-100" />
+              <Text>{text}</Text>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/services/kidsYogaData.js
@@ -0,0 +1,49 @@
+export const KIDS_YOGA_DATA = {
+  BENEFITS: [
+    {
+      title: "Yoga Helps Kids",
+      text: "The kids develop body awareness and learn how to use their bodies in a healthy way. It helps them manage stress through breathing, awareness, meditation, and healthy movement. They are able to build concentration and increase their confidence and create a positive self-image. The kids participating in yoga are able to feel part of a healthy, non-competitive group while giving them alternative to constantly being attached to electronic devices.",
+    },
+    {
+      title: "Kids Yoga Also Helps Benefits Teachers",
+      text: "The teachers can have an alternative way to handle challenges they may face in the classroom. It also gives the teachers a healthy activity to integrate into the daily lesson plan while benefiting from blending exercise into the class.",
+    },
+  ],
+  EXPECTATIONS: [
+    {
+      title: "Awareness of Breath",
+      imgName: "kids-yoga-breath.jpg",
+      text: "Breathing exercises can energize kids or encourage relaxation, depending on what you teach. Different games and techniques help kids connect to how their bodies feel as a result of deep breathing. Focus increases, as does their breathing and lung capacity. Stress is naturally reduced and healthy hormones are released.",
+    },
+    {
+      title: "Strengthening and Energizing",
+      imgName: "kids-yoga-strength.jpg",
+      text: "Kids think that yoga is great for stretching, but doesn't build strength. It's important for a teacher to include conversations, as well as exercises around how helpful yoga is for building strength. Talking about the different muscles used in poses and incorporating games and sequences will help build strength as well as body awareness and coordination. Bodies that are strong digest food better, maintain a healthy weight and can support the stress of carrying heavy loads, like a backpack. Bodies will also breathe better, work more efficiently and protect the more fragile joints.",
+    },
+    {
+      title: "Balancing",
+      imgName: "kids-yoga-balancing.jpg",
+      text: "Balancing poses teach children that with increased focus, you can increase attention naturally, even in kids who struggle with different attention challenges. Poses and games focused on balancing skills, develop an intrinsic strength, evoke a meditative feeling, and promote stillness and quieting of the mind. This can help kids deal with the stress of living in a chaotic world where constant stimulation is a regular part of life.",
+    },
+    {
+      title: "Stretching and Lengthening",
+      imgName: "kids-yoga-stretching.jpg",
+      text: "It's great for kids to be strong, but a body that's only based on strength has no way to yield under pressure. Strong muscles without accompanying flexibility can't move quickly, pulling on bones and joints. Yoga poses stretch muscles and through integrating breathing and movement, muscles become warm and become more flexible. They can yield when they need to, and support tender joints in a more functional way.",
+    },
+    {
+      title: "Awareness and Focus",
+      imgName: "kids-yoga-focus.jpg",
+      text: "Yoga helps create awareness in the body through deep breathing and movement. It gives kids a way to express themselves, build a strong connection between what they hear and what they do. Children that have healthy body awareness are more confident and strong, have better posture, breathe better and have a sense of quiet strength.",
+    },
+    {
+      title: "Flowing, Connecting, and Integrating",
+      imgName: "kids-yoga-flowing.jpg",
+      text: "When we string poses together, we give kids a taste of what it means to move with ease. It also helps them build the awareness that all our movements are a series of coordinated efforts between muscles, bones, joints, and nerves. Older kids are more able to isolate different muscle groups and get more sophisticated about movements; things like keeping the arms lifted in Warrior 1, while at the same time, dropping the shoulders to relax them. All these things together increase a child's sense of feeling integrated.",
+    },
+    {
+      title: "Meditation and Relaxation",
+      imgName: "kids-yoga-meditation.jpg",
+      text: "Yoga is meditative by nature. So whether a child is holding a balancing posture, sitting in meditation, or moving through a series of poses, there's going to be a calming, soothing quality. Giving younger kids something to do as they rest on their mats will help with their attention, such as suggesting they think of a favorite color or toy. Older kids will find it easier to rest longer with less structure.",
+    },
+  ],
+};
