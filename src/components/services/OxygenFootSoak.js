c414c6c8f322f3d557387420e25691fda581b6cb
Xavier Salazar <xavierssalazar92@gmail.com>
2023-01-20 16:07:09 -0600

    updating oxygen foot soak

--- src/components/services/OxygenFootSoak.js
+++ src/components/services/OxygenFootSoak.js
@@ -1,7 +1,8 @@
 import { useQuery } from "@apollo/client";
 import { GET_SERVICES_BY_CATEGORY } from "../../graphql/queries";
-import { Error, ServiceCard, SkeletonLoader } from "../shared";
+import { Error, Section, ServiceCard, SkeletonLoader } from "../shared";
 import { IMG_PATH_PREFIXES } from "../utils/imgPathPrefixes";
+import { OXYGEN_FOOT_SOAK_DATA } from "./oxygenFootSoakData";
 
 const OxygenFootSoak = ({ categoryId }) => {
   const { loading, error, data } = useQuery(GET_SERVICES_BY_CATEGORY, {
@@ -21,14 +22,30 @@ const OxygenFootSoak = ({ categoryId }) => {
   const { servicesByCategory } = data;
 
   return (
-    <div className="flex flex-wrap gap-6" id="oxygen-foot-soak">
-      {servicesByCategory.map((service) => (
-        <ServiceCard
-          key={service.id}
-          service={service}
-          imgPathPrefix={IMG_PATH_PREFIXES.SERVICES}
-        />
-      ))}
+    <div id="oxygen-foot-soak">
+      <div className="flex flex-wrap gap-6">
+        {servicesByCategory.map((service) => (
+          <ServiceCard
+            key={service.id}
+            service={service}
+            imgPathPrefix={IMG_PATH_PREFIXES.SERVICES}
+          />
+        ))}
+      </div>
+      <div className="flex flex-wrap gap-6 mt-12" id="ir-benefits">
+        {OXYGEN_FOOT_SOAK_DATA.map(({ title, text }) => (
+          <div
+            key={title}
+            className="card border-none rounded-2xl flex-1 basis-full md:basis-1/3 lg:basis-1/4"
+          >
+            <h1 className="text-3xl font-extra-bold lg:text-4xl">{title}</h1>
+            <hr className="w-2/4 md:w-6/12 xl:w-3/12 mx-auto my-4 border-slate-100" />
+            {text.map((data, i) => (
+              <Section key={i}>{data}</Section>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/services/oxygenFootSoakData.js
@@ -0,0 +1,15 @@
+export const OXYGEN_FOOT_SOAK_DATA = [
+  {
+    title: "Purify Your Body",
+    text: [
+      "Is the ionic foot bath machine right for me? With the increase in the number of chemicals and hazardous materials introduced in the last few decades, there has been a dramatic increase in incidences of auto-immune disease, allergies and common infections. Although people are living longer, we face new types of diseases and infections as we become immune to older ones. There are tens of thousands of chemicals in use today and more than a forth of these are known to be toxic and nothing is known about their interactive effects. Body fat has been tested that contains residue of hundreds of chemicals. Metals, such as mercury and cadmium are everywhere and impossible to avoid. Lead can block red blood cell formation.",
+      "Chemicals can act as toxins that can block receptor sites on cells, and cause changes in calcium homeostasis selectively killing cells, and alter expression of gene products. Metals and chemical toxins weaken the immune system and cause increased vulnerability to virus, bacterial fungal and parasitic infections. In an attempt to detoxify these substances, our bowels, kidneys and liver are being overloaded. Our eliminative channels have become blocked or dysfunctional, because, in conjunction with these synthetic chemicals, and the western diet that is so acid forming and de-vitalized.",
+    ],
+  },
+  {
+    title: "Benefits of a Oxygen Foot Soak",
+    text: [
+      "To relieve the strains on the kidneys and liver it is more important than ever to maintain a detoxification regimen. The ion foot bath is designed to relieve these strains and help the body re-balance its energy fields allowing organs to function better. After using ionic foot bath machine healthy individuals and users can feel lighter and experience a greater feeling of well being from each session. More energy and youth is felt as well. Some people with pain, edema, swollen, and deteriorating joints have also reported symptomatic relief from the sessions. Many doctors, nurses, professionals and the likes have also recommended this machine for their patients. We also found many excerpts stating that the machine has helped individuals break free from addiction problems.",
+    ],
+  },
+];
